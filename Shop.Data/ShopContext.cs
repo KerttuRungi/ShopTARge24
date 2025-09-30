c94cfb3c96fb6a13b4cc3461d26a2e2297c1198c
KerttuRungi <kerttu.rungi@gmail.com>
2025-09-30 11:40:43 +0300

    Edit controller before migration

--- Shop.Data/ShopContext.cs
+++ Shop.Data/ShopContext.cs
@@ -14,5 +14,7 @@ namespace Shop.Data
             : base(options) { }
         public DbSet<Spaceships> Spaceships { get; set; }
         public DbSet<FileToApi> FileToApis { get; set; }
+
+        public DbSet<RealEstate> RealEstate { get; set; }
     }
 }
--- Shop/Controllers/RealEstateController.cs
+++ Shop/Controllers/RealEstateController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models.RealEstate;
+using Shop.Core.Domain;
 
 namespace Shop.Controllers
 {
     public class RealEstateController : Controller
     {
+        private readonly ShopContext _context;
+
+        public RealEstateController(ShopContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+            var result = _context.RealEstate
+                .Select(x => new RealEstateIndexViewModel
+                {
+                    Id = x.Id,
+                    Area = x.Area,
+                    Location = x.Location,
+                    RoomNumber = x.RoomNumber,
+                    BuildingType = x.BuildingType,
+                    CreatedAt = x.CreatedAt,
+                    ModifiedAt = x.ModifiedAt
+                });
+
+            return View(result);
         }
     }
 }
+
