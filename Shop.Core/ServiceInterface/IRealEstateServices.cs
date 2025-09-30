b7526705764394b01c7b0627a987d22f64a0425c
KerttuRungi <kerttu.rungi@gmail.com>
2025-09-30 11:03:49 +0300

    Add RealEstate View

--- Shop.Core/ServiceInterface/IRealEstateServices.cs
+++ Shop.Core/ServiceInterface/IRealEstateServices.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Shop.Core.ServiceInterface;
 
-namespace Shop.ApplicationServices.Services
+namespace Shop.Core.ServiceInterface
 {
-    public class RealEstateServices : IRealEstateServices
+    public class IRealEstateServices
     {
     }
 }
