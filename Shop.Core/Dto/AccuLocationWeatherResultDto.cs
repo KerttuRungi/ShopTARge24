b8bbbac61fce55729332e1c29e21c261378fdc9b
KerttuRungi <kerttu.rungi@gmail.com>
2025-10-13 10:56:26 +0300

    AccuWeather in progress
    Changes that got left behind in the last commit

--- Shop.Core/Dto/AccuLocationWeatherResultDto.cs
+++ Shop.Core/Dto/AccuLocationWeatherResultDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Shop.Core.Dto
 {
     public class AccuLocationWeatherResultDto
