245636b28afe97dad8a6f641633cf1328afec1c9
KerttuRungi <kerttu.rungi@gmail.com>
2025-10-21 16:08:14 +0300

    Base for Chuck Norris Jokes API

---
+++ Shop/Models/ChuckNorris/ChuckNorrisViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Models.ChuckNorris
+{
+    public class ChuckNorrisViewModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
